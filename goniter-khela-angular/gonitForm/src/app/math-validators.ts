c9f83cc4e1ff54906d62e6da247bc2d54bfd22b2
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-02-24 17:21:05 +0900

    implemented a reusable validators.

--- goniter-khela-angular/gonitForm/src/app/math-validators.ts
+++ goniter-khela-angular/gonitForm/src/app/math-validators.ts
@@ -1,12 +1,15 @@
 import { AbstractControl } from "@angular/forms";
 
 export class MathValidators {
-    static addition(form: AbstractControl) {
-        const { a, b, answer } = form.value
-        if ( a + b === parseInt(answer) ) {
-            return null
+    static addition(target: string, sourceOne: string, secondSource: string) {
+        return (form: AbstractControl) => {
+            const sum = form.value[target]
+            const firstNumber = form.value[sourceOne]
+            const secondNumber = form.value[secondSource]
+            if ( firstNumber + secondNumber === parseInt(sum) ) {
+                return null
+            }
+            return { Addition: true} 
         }
-        return { Addition: true }
-
     }
 }
