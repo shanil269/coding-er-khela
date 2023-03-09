f5b8c3b801e307aacc53a891a81bef5bf6b33065
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-03-09 21:30:40 +0900

    refined behavior subject in html

--- email-client-angular/emailclient/src/app/app.component.ts
+++ email-client-angular/emailclient/src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { SignupService } from './auth/service/signup.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  signedin = false
+export class AppComponent {
+  signedin$: BehaviorSubject<boolean>
 
-  constructor(private signup: SignupService) { }
-
-  ngOnInit(): void {
-    this.signup.signedin$.subscribe((signedin) => {
-      this.signedin = signedin
-    })
+  constructor(private signup: SignupService) {
+    this.signedin$ = signup.signedin$
   }
+
 }
