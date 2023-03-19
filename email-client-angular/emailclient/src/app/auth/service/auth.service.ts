edafbcbf09246f98ef2c696ba3733e721a5655e5
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-03-19 19:30:16 +0900

    added sign in authentication

--- email-client-angular/emailclient/src/app/auth/service/auth.service.ts
+++ email-client-angular/emailclient/src/app/auth/service/auth.service.ts
@@ -22,6 +22,11 @@ interface SignedinResponse {
   username: string;
 }
 
+export interface SigninCredentials {
+  username: string,
+  password: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -61,6 +66,14 @@ export class AuthService {
       .pipe(
         tap(() => {
           this.signedin$.next(false)
+        })
+      )
+  }
+  signin(credentials: SigninCredentials) {
+    return this.http.post(`${this.rootUrl}/auth/signin`, credentials)
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true)
         })
       )
   }
--- email-client-angular/emailclient/src/app/auth/signin/signin.component.ts
+++ email-client-angular/emailclient/src/app/auth/signin/signin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { SigninCredentials } from '../service/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -21,9 +23,17 @@ export class SigninComponent implements OnInit {
     ])
   })
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return
+    }
+
+    this.authService.signin(this.authForm.value as SigninCredentials).subscribe(() => { })
   }
 
 }
