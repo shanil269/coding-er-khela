b3885146479509fd39ee0f643c2ceef8e9181903
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-03-19 18:51:36 +0900

    added programatic signout

--- email-client-angular/emailclient/src/app/auth/signout/signout.component.ts
+++ email-client-angular/emailclient/src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signout',
@@ -8,10 +9,15 @@ import { AuthService } from '../service/auth.service';
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.authService.signout().subscribe(() => { })
+    this.authService.signout().subscribe(() => {
+      this.router.navigateByUrl('/')
+    })
   }
 
 }
