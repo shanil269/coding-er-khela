ed28fae88fb41c5e60e29b30798a2d84798f65b8
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-03-22 20:17:51 +0900

    fixed broken navigation in auth guard while doing the inbox routing

--- email-client-angular/emailclient/src/app/auth/auth.guard.ts
+++ email-client-angular/emailclient/src/app/auth/auth.guard.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
-import { map, Observable, skipWhile, take } from 'rxjs';
+import { CanLoad, Route, UrlSegment, UrlTree, Router } from '@angular/router';
+import { map, Observable, skipWhile, take, tap } from 'rxjs';
 import { AuthService } from './service/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.signedin$.pipe(
       skipWhile((value) => value === null),
       map((value) => value!),
-      take(1)
+      take(1),
+      tap(authenticated => {
+        if (!authenticated) {
+          return this.router.navigateByUrl('/')
+        }
+        else {
+          return
+        }
+      })
     )
   }
 }
